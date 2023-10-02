e8401698effd1e3bd0520b9b9ae6d1ffcaa0f8a4
zhenyuWang <13641039885@163.com>
2023-10-02 21:05:07 +0800

    feat: post detail page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,3 +1,36 @@
-export default function PostDetailPage() {
-  return <h1>Post Detail Page</h1>
+import PostContent from '@/components/posts/post-detail/post-content'
+import { getPostData, getPostsFiles } from '@/lib/posts-util'
+
+export default function PostDetailPage({ postData }) {
+  return <PostContent post={postData} />
+  // return <h1>post detail page</h1>
+}
+
+export function getStaticProps({ params }) {
+  const { slug } = params
+  const postData = getPostData(slug)
+
+  return {
+    props: {
+      postData,
+    },
+    revalidate: 600,
+  }
+}
+
+export function getStaticPaths() {
+  const filenames = getPostsFiles()
+  const paths = filenames.map((filename) => {
+    return {
+      params: {
+        slug: filename.replace('.md', ''),
+      },
+    }
+  })
+
+  return {
+    paths,
+    fallback: false,
+    // fallback: true, | blocking
+  }
 }
