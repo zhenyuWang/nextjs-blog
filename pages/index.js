6fe84355b5c91d0dfcd9d8ea22754647a2c924f9
zhenyuWang <13641039885@163.com>
2023-10-02 21:02:42 +0800

    feat: home page with md data

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,23 @@
 import Hero from '@/components/home-page/hero'
 import FeaturedPosts from '@/components/home-page/featured-posts'
+import { getFeaturedPosts } from '@/lib/posts-util'
 
-export default function HomePage() {
-  const DUMMY_POSTS = [
-    {
-      slug: 'getting-started-with-nextjs1',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-    {
-      slug: 'getting-started-with-nextjs2',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-    {
-      slug: 'getting-started-with-nextjs3',
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2022-02-10',
-    },
-  ]
+export default function HomePage({ featuredPosts }) {
   return (
     <>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={featuredPosts} />
     </>
   )
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts()
+
+  return {
+    props: {
+      featuredPosts,
+      // revalidate: 60,
+    },
+  }
 }
