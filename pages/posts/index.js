8184643a505d93618ec3a3ef3b4cc1fe22fa0515
zhenyuWang <13641039885@163.com>
2023-10-02 21:03:40 +0800

    feat: all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,16 @@
-export default function PostPage() {
-  return <h1>Post Page</h1>
+import AllPosts from '@/components/posts/all-posts'
+import { getAllPosts } from '@/lib/posts-util'
+
+export default function AllPostsPage({ posts }) {
+  return <AllPosts posts={posts} />
+}
+
+export function getStaticProps() {
+  const posts = getAllPosts()
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
